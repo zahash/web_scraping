902fa929fc5ff9f2de124d0ec51a5661445cdb44
bigdata <bigdata@localhost.localdomain>
2019-12-02 15:29:29 +0530

    lab1.py : scraped all the quotes and authors from quotes.toscrape.com

--- lab1.py
+++ lab1.py
@@ -6,6 +6,12 @@ if __name__ == '__main__':
     url = r'http://quotes.toscrape.com/'
     response = urllib.request.urlopen(url)
     html_page = response.read()
-    soup = BeautifulSoup(html_page)
-    print(soup.prettify())
-
+    soup = BeautifulSoup(html_page, features="html.parser")
+    #print(soup.prettify())
+    
+    for div in soup.find_all("div", attrs={"class" : "quote"}):
+        quote = div.find("span", attrs={"class" : "text"}).text
+        author = div.find("small", attrs={"class" : "author"}).text
+        print("quote:  ", quote)
+        print("author: ", author)
+        print('\n')
