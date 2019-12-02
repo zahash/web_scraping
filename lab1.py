834f9fc20bdd8314116afb86bc0ad6f1a93136fe
bigdata <bigdata@localhost.localdomain>
2019-12-02 15:13:55 +0530

    lab1.py : change the website url

--- lab1.py
+++ lab1.py
@@ -3,8 +3,9 @@ from bs4 import BeautifulSoup
 import urllib
 
 if __name__ == '__main__':
-    url = r'https://pypi.org/project/beautifulsoup4/'
+    url = r'http://quotes.toscrape.com/'
     response = urllib.request.urlopen(url)
     html_page = response.read()
     soup = BeautifulSoup(html_page)
     print(soup.prettify())
+
